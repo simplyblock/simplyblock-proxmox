ac59bdb902ae11f3ab1cba6373d2ab3fcddab90a
Max Schettler <max@simplyblock.io>
2025-03-05 11:29:17 +0000

    Support containers

--- SimplyblockPlugin.pm
+++ SimplyblockPlugin.pm
@@ -127,8 +127,8 @@ sub type {
 sub plugindata {
     return {
         content => [
-            { images => 1 },
-            { images => 1 }
+            { images => 1, rootdir => 1 },
+            { images => 1, rootdir => 1 }
         ],
         format => [
             { raw => 1 },

ccf64e6267ea40c1877e11465fa1da843fcca988
Max Schettler <max@simplyblock.io>
2026-06-18 16:15:24 +0200

    Make images configurable for tests

--- test/conftest.py
+++ test/conftest.py
@@ -8,6 +8,13 @@ def pytest_addoption(parser):
     parser.addoption("--cluster", action="store", required=True)
     parser.addoption("--secret", action="store", required=True)
     parser.addoption("--pool", action="store", required=True)
+    parser.addoption("--vm-image", action="store", default=None,
+                     help="Path to a VM disk image (e.g. /root/debian.qcow2); "
+                          "VM tests are skipped when omitted")
+    parser.addoption("--ct-template", action="store", default=None,
+                     help="Proxmox container template volid "
+                          "(e.g. local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst); "
+                          "container tests are skipped when omitted")
 
 
 @pytest.fixture(scope='session')
@@ -24,3 +31,13 @@ def storage(request):
     ])
     yield name
     co(['pvesm', 'remove', name])
+
+
+@pytest.fixture(scope='session')
+def vm_image(request):
+    return request.config.option.vm_image
+
+
+@pytest.fixture(scope='session')
+def ct_template(request):
+    return request.config.option.ct_template
